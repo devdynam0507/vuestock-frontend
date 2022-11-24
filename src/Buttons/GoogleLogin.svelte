e308a1c368df7b7c676b4aad4e2a94b95692b28a
devdynam0507 <wsnam0507@naver.com>
2022-11-24 23:33:41 +0900

    feat: landing 페이지

---
+++ src/Buttons/GoogleLogin.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let src = 'images/google-signin.png';
+</script>
+
+<div id="login-button">
+    <img {src} alt="google"/>
+</div>
+
+<style>
+    #login-button {
+		cursor: pointer;
+	}
+</style>
---
+++ src/Components/ImageComponent.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let src: string;
+    export let width: string;
+    export let height: string;
+    export let alt: string;
+</script>
+
+<div width={width} height={height}>
+    <img {src} {alt}/>
+</div>
